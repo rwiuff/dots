d4c27b3bf5bf46fd8bea1bdbca70fef243d24867
Rasmus Wiuff <rwiuff@gmail.com>
2019-03-05 14:12:22 +0100

    Modified username

--- blues/.config/polybar/launch.sh
+++ blues/.config/polybar/launch.sh
@@ -7,5 +7,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar main -c /home/aditya/.config/polybar/config &
+polybar main -c /home/rwiuff/.config/polybar/config &
 #polybar sec -c /home/aditya/.config/polybar/config.sec &
